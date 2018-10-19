64c058722dd6ddc34cce7aec72c5a61b47f58350
enra4 <cudcsb@gmail.com>
2018-10-19 19:47:20 +0200

    add logging

--- src/nut.cr
+++ src/nut.cr
@@ -1,8 +1,12 @@
 require "kemal"
+require "reeelog"
 require "./utilities.cr"
 
+# you might have to set headers in nginx or something if u want ip addresses in logs
 # Kemal.config.env = "production"
 Kemal.config.public_folder = "./uploads"
+logging false # disable kemal logging
+log = Reeelog.start("nut.log")
 PORT = 3000
 SECRET = "" # replace this with your own secret :)
 EXTENSIONS = {
@@ -15,7 +19,13 @@ get "/" do |env|
 end
 
 post "/upload" do |env|
+	ip = "someone"
+	if env.request.headers.has_key?("X-Forwarded-For")
+		ip = env.request.headers["X-Forwarded-For"]
+	end
+
 	if env.request.headers["secret"] != SECRET
+		log.warn("upload", "#{ip} tried to upload with wrong secret")
 		halt(env, status_code: 401, response: "unauthorized")
 	end
 
@@ -23,17 +33,25 @@ post "/upload" do |env|
 	extension = ""
 	HTTP::FormData.parse(env.request) do |upload|
 		if Utilities.illegal_extension?(upload.filename, EXTENSIONS)
+			log.warn("upload", "#{ip} tried to upload file with illegal extension")
 			halt(env, status_code: 403, response: "forbidden")
 		else
 			extension = Utilities.find_extension(upload.filename)
 			id = Utilities.generate_id
-			File.open("uploads/#{id}.#{extension}", "w") do |file|
-				IO.copy(upload.body, file)
-			end
+			begin
+				File.open("uploads/#{id}.#{extension}", "w") do |file|
+					IO.copy(upload.body, file)
+				end
 
-			"#{id}.#{extension}"
+				log.success("upload", "#{ip} uploaded to uploads/#{id}.#{extension}")
+				halt(env, status_code: 200, response: "#{id}.#{extension}")
+			rescue error
+				log.error("upload", "error writing to file (error from #{ip})")
+				halt(env, status_code: 500, response: "something broke")
+			end
 		end
 	end
 end
 
+log.info("app", "server is running on port #{PORT.to_s}")
 Kemal.run(PORT)
